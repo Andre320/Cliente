35b292a8687792d612b55fc9742a269e9a9eb070
Andre Salazar <andresalaeli.13@gmail.com>
2021-06-15 18:30:14 -0600

    Add project files.

---
+++ Juego.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace ClienteVentanas
+{
+    partial class Juego
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelTablero = new System.Windows.Forms.Panel();
+            this.panelInicio = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.panelInicio.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelTablero
+            // 
+            this.PanelTablero.BackColor = System.Drawing.Color.Silver;
+            this.PanelTablero.Location = new System.Drawing.Point(12, 12);
+            this.PanelTablero.Name = "PanelTablero";
+            this.PanelTablero.Size = new System.Drawing.Size(914, 912);
+            this.PanelTablero.TabIndex = 1;
+            this.PanelTablero.Visible = false;
+            // 
+            // panelInicio
+            // 
+            this.panelInicio.Controls.Add(this.comboBox1);
+            this.panelInicio.Controls.Add(this.label3);
+            this.panelInicio.Controls.Add(this.label2);
+            this.panelInicio.Controls.Add(this.txtNombre);
+            this.panelInicio.Controls.Add(this.label1);
+            this.panelInicio.Controls.Add(this.txtIP);
+            this.panelInicio.Controls.Add(this.btnConectar);
+            this.panelInicio.Location = new System.Drawing.Point(932, 12);
+            this.panelInicio.Name = "panelInicio";
+            this.panelInicio.Size = new System.Drawing.Size(388, 239);
+            this.panelInicio.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Rojo",
+            "Amarillo",
+            "Morado",
+            "Blanco"});
+            this.comboBox1.Location = new System.Drawing.Point(161, 117);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(125, 28);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(93, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Color:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(161, 65);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(125, 27);
+            this.txtNombre.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(117, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IP:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(161, 17);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(125, 27);
+            this.txtIP.TabIndex = 1;
+            this.txtIP.Text = "25.107.59.124";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(161, 187);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(94, 29);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // Juego
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1882, 953);
+            this.Controls.Add(this.panelInicio);
+            this.Controls.Add(this.PanelTablero);
+            this.Name = "Juego";
+            this.Text = "Juego";
+            this.Load += new System.EventHandler(this.Juego_Load);
+            this.panelInicio.ResumeLayout(false);
+            this.panelInicio.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelTablero;
+        private System.Windows.Forms.Panel panelInicio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ Juego.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+namespace ClienteVentanas
+{
+    public partial class Juego : Form
+    {
+        PictureBox[,] matrizTablero = new PictureBox[10, 10];
+        public Juego()
+        {
+            InitializeComponent();
+
+            crearMatriz();
+
+            mostrarMatrizTablero();
+        }
+
+        private void Juego_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        //creacion y visualizacion de la matriz de picBox
+        public void crearMatriz()
+        {
+         
+            int posX = 3;
+            int posY = 3;
+
+            for (int i=0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    PictureBox pic = new PictureBox();
+                    pic.Name = "picBox_"+i+"_"+j;
+                    pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pic.BorderStyle = BorderStyle.Fixed3D;
+                    pic.Size = new Size(85,85);
+                    pic.Location = new Point(posX,posY);
+                    matrizTablero[i,j] = pic;
+                    if (j == 9)
+                    {
+                        posX = 3;
+                    }
+                    else { posX = posX + 91; }
+                    
+                }
+
+                if (i == 9)
+                {
+                    posY = 3;
+                }
+                else { posY = posY + 91; }
+            }
+
+        }
+
+        public void mostrarMatrizTablero()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    PanelTablero.Controls.Add(matrizTablero[i, j]);
+
+                }
+
+              
+            }
+        }
+
+
+        //conexion al server
+        static private NetworkStream stream;
+        static private StreamWriter writter;
+        static private StreamReader reader;
+        static private TcpClient cliente = new TcpClient();
+
+        public void Escuchar()
+        {
+            while (cliente.Connected)
+            {
+                try
+                {
+                    Debug.WriteLine("mensaje del servidor: " + reader.ReadLine());
+
+
+                }
+                catch { }
+            }
+        }
+
+
+
+        public void Conectar()
+        {
+            try
+            {
+                
+                cliente.Connect(IPAddress.Parse(txtIP.Text), int.Parse("46000"));
+                if (cliente.Connected)
+                {
+
+                    PanelTablero.Visible = true;
+                    panelInicio.Visible = false;
+                    string color = "";
+                    string nombre =txtNombre.Text;
+                    string data = "Hola Mundo";
+
+                    Debug.WriteLine("Conectado con exito");
+
+                    Thread thread = new Thread(Escuchar);
+
+                    stream = cliente.GetStream();
+                    writter = new StreamWriter(stream);
+                    reader = new StreamReader(stream);
+
+                    writter.WriteLine(color);
+                    writter.WriteLine(nombre);
+                    writter.WriteLine(data);
+
+                    writter.Flush();
+
+                    thread.Start();
+
+
+                }
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine(e);
+                MessageBox.Show("se murio");
+                  
+            }
+        }
+
+        public void Enviar(string mensaje)
+        {
+            writter.WriteLine(mensaje);
+            writter.Flush();
+        }
+
+       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            Conectar();
+        }
+    }
+}
